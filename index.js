659c88ea11a24e1f84eade78025d05c15ce37c0a
JonasKiwia <jonas.kiwia@student.moringaschool.com>
2024-11-27 12:18:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+// Define companyName as a constant with the value "Scuber"
+const companyName = "Scuber";
+
+// Define mostProfitableNeighborhood with the value specified in the tests
+let mostProfitableNeighborhood = "Chelsea"; // Replace with the value specified in the tests
+
+// Define companyCeo with the value specified in the tests
+let companyCeo = "Susan Smith"; // Replace with the value specified in the tests
